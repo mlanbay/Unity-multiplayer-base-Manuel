93a2471c8d09a3ebffbed24c416628d9af6ad481
mlanbay <lancharro356@gmail.com>
2025-02-06 11:38:16 +0100

    comit

--- Assets/AvatarController.cs
+++ Assets/AvatarController.cs
@@ -1,32 +1,49 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class AvatarController : MonoBehaviour
 {
     [SerializeField] private float speed = 3.0f;
     private SpriteRenderer spriteRenderer;
+    private Vector2 inputVector;
+    private Rigidbody2D rigidbody2D;
+    
+    private bool keydown;
     
     // crea un avatar (Cliente) y le asigna un color distinto
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();    
     }
 
     // controla la posicion de los clientes
     void Update()
     {
-        
+        Move();
     }
+    
+    
     
     //maneja el input del Cliente que posees
 
-    public void Move(InputAction.CallbackContext context)
+    public void OnMoveInput(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        inputVector = context.ReadValue<Vector2>();
+        if (context.canceled)
         {
-            Vector2 movement = context.ReadValue<Vector2>();
-            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+            keydown = false;
+        }else if (context.started)
+        {
+            keydown = true;
         }
     }
-    
+
+    public void Move()
+    {
+        if (keydown)
+        {
+            rigidbody2D.linearVelocity = inputVector * (speed * Time.deltaTime);
+        }
+    }
 }
